8884b2f7377ac7327e7bf2962ffe0874eef2af34
hogewest <hogewest@gamil.com>
2024-06-22 21:14:37 +0900

    Mod application.rb

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,7 @@ require_relative "boot"
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
-# require "active_job/railtie"
+require "active_job/railtie"
 require "active_record/railtie"
 # require "active_storage/engine"
 require "action_controller/railtie"
@@ -11,7 +11,7 @@ require "action_controller/railtie"
 # require "action_mailbox/engine"
 # require "action_text/engine"
 require "action_view/railtie"
-# require "action_cable/engine"
+require "action_cable/engine"
 require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -37,5 +37,6 @@ module Holohato
     # config.eager_load_paths << Rails.root.join("extras")
 
     config.action_dispatch.default_headers["Referrer-Policy"] = "same-origin"
+    config.active_record.sqlite3_production_warning=false
   end
 end
