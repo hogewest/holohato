4e892cfa2c2c50d49c4df00f7e2cd45cd81a5de8
hogewest <hogewest@gmail.com>
2024-07-08 23:20:55 +0900

    Mod kaminari_config.rb

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -3,7 +3,7 @@
 Kaminari.configure do |config|
   config.default_per_page = 10
   # config.max_per_page = nil
-  config.window = 3
+  config.window = 2
   # config.outer_window = 0
   config.left = 1
   config.right = 1
