a4e5889897871c4edcfbf455f45caf3cd7ea3163
hogewest <hogewest@gmail.com>
2024-08-25 14:08:03 +0900

    Mod html title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,6 @@
 module ApplicationHelper
+  BASE_TITLE = "Holohato"
+
   def param_blank?(param)
     param.blank? || strip_space(param).empty?
   end
@@ -6,5 +8,9 @@ module ApplicationHelper
   def strip_space(str)
     return "" if str.nil?
     str.gsub(/[[:space:]]/, "")
+  end
+
+  def page_title(page_title = "")
+    page_title.empty? ? BASE_TITLE : page_title + ' - ' + BASE_TITLE
   end
 end
