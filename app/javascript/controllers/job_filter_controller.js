431603c7687617d64f5aa389980187ed71faa59a
hogewest <hogewest@gmail.com>
2024-11-16 14:40:10 +0900

    Update job page

--- app/javascript/controllers/job_filter_controller.js
+++ app/javascript/controllers/job_filter_controller.js
@@ -4,7 +4,8 @@ export default class extends Controller {
   onChangeState(event) {
     const url = new URL(location.href);
     const params = new URLSearchParams(location.search);
-    params.set("state", event.target.value)
+    params.set("state", event.target.value);
+    params.delete("p");
     url.search = params.toString();
     window.location = url.href;
   }
